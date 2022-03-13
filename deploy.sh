e5d31740fdd0d120976dd324b16ec58776443ce1
augchan42 <auchan@yahoo.com>
2022-03-13 18:12:05 +0800

    add plug integration

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-dfx deploy --argument '("IC_NERDS","NERDS")'
+#dfx deploy --argument '("IC_NERDS","NERDS")'
+dfx deploy --network ic --argument '("IC_NERDS","NERDS")'
--- src/minter_assets/src/index.js
+++ src/minter_assets/src/index.js
@@ -4,9 +4,14 @@ import { minter } from "../../declarations/minter";
 // This is library to use with principal that is provided by Dfinity
 import { Principal } from "@dfinity/principal";
 
+import PlugConnect from '@psychedelic/plug-connect';
+
 // For beginners : This is really basic Javascript code that add an event to the "Mint" button so that the mint_nft function is called when the button is clicked.
 const mint_button = document.getElementById("mint");
 mint_button.addEventListener("click", mint_nft);
+
+const connect_button = document.getElementById("connect");
+connect_button.addEventListener("click", connect_plug_wallet);
 
 async function mint_nft() {
   // Get the url of the image from the input field
@@ -14,7 +19,7 @@ async function mint_nft() {
   console.log("The url we are trying to mint is " + name);
 
   // Get the principal from the input field.
-  const principal_string = document.getElementById("principal").value.toString();
+  const principal_string = document.getdsaElementById("principal").value.toString();
   const principal = Principal.fromText(principal_string);
 
   // Mint the image by calling the mint_principal function of the minter.
@@ -28,3 +33,28 @@ async function mint_nft() {
   // Show some information about the minted image.
   document.getElementById("greeting").innerText = "this nft owner is " + principal_string + "\nthis token id is " + Number(mintId);
 }
+
+async function connect_plug_wallet() {
+  // const publicKey = await window.ic.plug.requestConnect();
+  // document.getElementById("principal").value = publicKey;
+
+  const nnsCanisterId = 'qumey-pqaaa-aaaai-abtla-cai'
+
+  // Whitelist
+  const whitelist = [
+    nnsCanisterId,
+  ];
+
+  // Make the request
+  const isConnected = await window.ic.plug.requestConnect({
+    whitelist,
+  });
+
+  // Get the user principal id
+  const principalId = await window.ic.plug.agent.getPrincipal();
+  document.getElementById("principal").value = principalId;
+
+  console.log(`Plug's user principal Id is ${principalId}`);
+}
+
+
